fe92538500147e9466662beb4fa5a7b63d9b28fe
Kevin Gustavo <kg.2k.pl@gmail.com>
2022-08-10 17:26:47 -0400

    creación de eventos para indicar los cambios en la lista de usuarios

---
+++ app/Events/UserDeleted.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserDeleted implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    public $user;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    //recibimos una instancia del modelo User
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    //indica donde vamos a transmitir esta información
+    public function broadcastOn()
+    {
+        // \Log::debug("{$this->message}");
+        // \Log::debug("{$this->type}");
+        return new Channel('users');
+    }
+}
